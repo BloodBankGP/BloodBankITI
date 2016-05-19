d9a2cd57d73ffb28d199cf2a2dd175565056ef98
MoahmedMagdy <mvcproject6@gmail.com>
2016-05-19 14:15:26 +0200

    WebApi

--- BloodBankService/Controllers/HomeController.cs
+++ BloodBankService/Controllers/HomeController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace BloodBankService.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
-        public ActionResult Index()
-        {
-            ViewBag.Title = "Home Page";
+        Models.BloodBankDBITIEntities db = new Models.BloodBankDBITIEntities();
 
-            return View();
+        [HttpGet]
+        public List<Models.posts_SelectAll_Result> AllPosts ()
+        {
+            var result = db.posts_SelectAll();
         }
     }
 }
