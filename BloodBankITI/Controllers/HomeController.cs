84bc4163d834888046c7d2b4947b8477e3750bf6
MoahmedMagdy <mvcproject6@gmail.com>
2016-05-30 16:10:19 +0200

    Home controller

--- BloodBankITI/Controllers/HomeController.cs
+++ BloodBankITI/Controllers/HomeController.cs
@@ -166,5 +166,58 @@ namespace BloodBankITI.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult Donate()
+        {
+
+            List<Cities_SelectAll_Result> cities = new List<Cities_SelectAll_Result>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://www.bloodservice.somee.com/Home/");
+            HttpResponseMessage response = client.GetAsync("ALLCities").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                cities = response.Content.ReadAsAsync<List<Cities_SelectAll_Result>>().Result;
+
+            }
+
+
+            List<Select_BloodTypes_Result> bloodTypes = new List<Select_BloodTypes_Result>();
+
+            response = client.GetAsync("AllBloodTypes").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                bloodTypes = response.Content.ReadAsAsync<List<Select_BloodTypes_Result>>().Result;
+
+            }
+
+            donorinsertform donor = new donorinsertform()
+            {
+                BloodTypesResults = bloodTypes,
+                Donor = null,
+                CitiesSelectAllResults = cities,
+                //LocationsSelectAllByCity = null
+            };
+            return View(donor);
+        }
+
+
+        [HttpPost]
+        public ActionResult Donate(Donor donor)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://www.bloodservice.somee.com/Home/");
+            HttpResponseMessage response = client.PostAsJsonAsync("donor_insert/donor", donor).Result;
+            string result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                result = "Done";
+            }
+            else
+                result = "Failed to insert Donor";
+
+            return RedirectToAction("Index");
+        }
     }
 }
