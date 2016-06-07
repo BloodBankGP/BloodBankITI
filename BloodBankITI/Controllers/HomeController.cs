a9797462f04d2daaeba2c7a6e412e2ff32a08ee4
HagarAbouAbdou <hagaraboabdo48@gmail.com>
2016-06-07 03:01:46 +0200

    <br>

--- BloodBankITI/Controllers/HomeController.cs
+++ BloodBankITI/Controllers/HomeController.cs
@@ -227,32 +227,30 @@ namespace BloodBankITI.Controllers
         }
 
 
-        [HttpPost]
-        public ActionResult Donate(Donor donor)
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://www.bloodservice.somee.com/Home/");
-            HttpResponseMessage response = client.PostAsJsonAsync("donor_insert/donor", donor).Result;
-            string result;
+        //[HttpPost]
+        //public ActionResult Donate(Donor donor)
+        //{
+        //    HttpClient client = new HttpClient();
+        //    client.BaseAddress = new Uri("http://www.bloodservice.somee.com/Home/");
+        //    HttpResponseMessage response = client.PostAsJsonAsync("donor_insert/donor", donor).Result;
+        //    string result;
 
-            if (response.IsSuccessStatusCode)
-            {
-                result = "Done";
-            }
-            else
-                result = "Failed to insert Donor";
-            if (donor.BID == 0)
-            {
-                return RedirectToAction("selectPartner",new {id=donor.DID});
-            }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        result = "Done";
+        //    }
+        //    else
+        //        result = "Failed to insert Donor";
+        //    if (donor.BID == 0)
+        //    {
+        //        return RedirectToAction("selectPartner",new {id=donor.DID});
+        //    }
     
-            return RedirectToAction("Index");
-        }
+        //    return RedirectToAction("Index");
+        //}
 
-        [HttpGet] 
-        public ActionResult selectPartner(int id)
-
-
+        //[HttpGet] 
+        //public ActionResult selectPartner(int id)
 
 
 
@@ -263,6 +261,7 @@ namespace BloodBankITI.Controllers
 
 
 
-        [HttpPost]
+
+
     }
 }
