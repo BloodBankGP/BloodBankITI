12a33ea93670698f50756e3de9bdcef5754a71d7
HagarAbouAbdou <hagaraboabdo48@gmail.com>
2016-06-20 21:22:41 +0200

    update

--- BloodBankITI/Models/BloodBankITI.Designer.cs
+++ BloodBankITI/Models/BloodBankITI.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Eman\Desktop\project\BloodBank_guthub\BloodBankITI\BloodBankITI\Models\BloodBankITI.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\BloodBankITI\BloodBankITI\Models\BloodBankITI.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
